ce18b7718225c6502c13be0f09cb2e319ef6f218
LJWill <javenli126@gmail.com>
2018-08-02 17:00:42 +1000

    update test_section

---
+++ public/templates/single-property/test.php
@@ -0,0 +1,20 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+global $post;
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      One of three columns
+    </div>
+    <div class="col-sm-4">
+      One of three columns
+    </div>
+    <div class="col-sm-4">
+      One of three columns
+    </div>
+  </div>
+</div>
